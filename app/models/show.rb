2ebb4ffca672066a292ff5b198d3e356f0d3c5be
Nick Nobile <nickcnobile@gmail.com>
2020-11-22 17:09:03 -0500

    commit tree d1f85e7160483af4c93642bf7cd1e71b9960dc72

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+        self.maximum(:rating)
+    end
+
+    def self.most_popular_show
+        show_rating = self.maximum(:rating)
+        self.find_by(rating: show_rating)
+    end
+
+    def self.lowest_rating
+        self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+        show_rating = self.minimum(:rating)
+        self.find_by(rating: show_rating)
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating > 5")
+    end
+
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+
+end
